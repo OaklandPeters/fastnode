4f1dc546d61ade3b0d9afb5603e1445329deff22
Oakland Peters <oakland.peters@gmail.com>
2015-05-02 12:42:34 -0400

    First commit

---
+++ fast/node.py
@@ -0,0 +1,12 @@
+"""
+
+The goal of this class is to provide the AST-like (Abstract Syntax Tree)
+behavior portion of several logical and syntactic structures.
+I find myself building these over and over again, and developing
+a solid, abstacted version would save a lot of time.
+... and be emotionally satisfying.
+
+"""
+
+class Node(object):
+	pass
